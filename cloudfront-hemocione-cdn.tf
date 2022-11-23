89fdcfcb3c1af7c7e2b05159d4290f7038a2d4f3
Bruno Dantas <bruno.d.paiva@hotmail.com>
2022-11-22 22:37:52 -0300

    feat(cloudfront-hemocione-cdn.tf): add hemocione cloudfront

---
+++ cloudfront-hemocione-cdn.tf
@@ -0,0 +1,95 @@
+resource "aws_cloudfront_origin_access_control" "hemocione-cdn-access-control" {
+  name                              = "hemocione-cdn-access-control"
+  description                       = "Policy to handle the origin access control for hemocione cloudfront"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "hemocione-cdn" {
+  origin {
+    domain_name              = aws_s3_bucket.hemocione-assets.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.hemocione-cdn-access-control.id
+    origin_id                = local.hemocione-cdn-origin-id
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "hemocione assets distribution cdn"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.hemocione-cdn-origin-id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.hemocione-cdn-origin-id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.hemocione-cdn-origin-id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_All"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
